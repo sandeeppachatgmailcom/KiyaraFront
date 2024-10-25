b278be8c10c1ae89b069fceaa37faa324d0dd9a1
sandeep <sandeepcheriyan@gmail.com>
2024-10-25 10:47:21 +0530

    ser changed https://kiarabackend.onrender.com

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 const nodeServer = axios.create({
-    baseURL: 'http://localhost:5000/'
+    baseURL: 'https://kiarabackend.onrender.com'
   });
 
   export default nodeServer
