6d9aed83590341178fde2898d6f2905486d55293
sandeep <sandeepcheriyan@gmail.com>
2024-10-26 19:18:20 +0530

    login component disabled

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,7 @@ const Login = ()=>{
     return(
         <div className="w-full   h-[100%] justify-center relative flex items-center">
         <div className="  w-[70%] h-[70%] flex justify-center items-center   xl:w-3/12 lg:end-12   rounded-md  ">
-            <LoginBox/>
+            {/* <LoginBox/> */}
         </div>
     </div>
     )
