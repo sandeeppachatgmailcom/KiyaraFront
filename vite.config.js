b5b4b654074f8018b5731e0fcd0ada74e3f868c5
sandeep <sandeepcheriyan@gmail.com>
2024-10-26 19:32:24 +0530

    vite congig modified

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
@@ -7,4 +7,5 @@ export default defineConfig({
     open: true,
     port: 3000,
   },
-});
+  base: './', // Add this line
+})
