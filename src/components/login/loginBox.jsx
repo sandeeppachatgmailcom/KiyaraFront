bd7f0c0b4a36400172db8ffab5349207b3fed8ca
sandeep <sandeepcheriyan@gmail.com>
2024-10-28 17:13:20 +0530

    login  credential added to login page

--- src/components/login/loginBox.jsx
+++ src/components/login/loginBox.jsx
@@ -14,62 +14,65 @@ const LoginBox = () => {
     const login = useLogin()
     const navigate = useNavigation()
     const dispatch = useDispatch()
-    const activeUser = useSelector((state)=>state.user.user)
-    const [user,setUser] = useState({
-        firstname:'',
-        email:'',
-        password:''
+    const activeUser = useSelector((state) => state.user.user)
+    const [user, setUser] = useState({
+        firstname: '',
+        email: '',
+        password: ''
     })
-    useEffect(()=>{
-         if (Object.keys(activeUser)?.length ) navigate('adminHome')
-    },[activeUser])
-    const handleChange = (event)=>{
-        const {name,value} = event.target
+    useEffect(() => {
+        if (Object.keys(activeUser)?.length) navigate('adminHome')
+    }, [activeUser])
+    const handleChange = (event) => {
+        const { name, value } = event.target
         const temp = {
             ...user,
-            [name]:value
+            [name]: value
         }
-        
+
         setUser(temp)
     }
-    const saveData =async ()=>{
+    const saveData = async () => {
         const result = await login(user)
-        if(result?.status){
-             
-            toast.success(result?.message );
+        if (result?.status) {
+
+            toast.success(result?.message);
             dispatch(userlogin(result))
-            
+
 
         }
-        else{
+        else {
             toast.error(result?.message)
-            
+
 
         }
-    } 
+    }
 
     return (
         <div className="w-[90%] border border-gray-400 border-opacity-30 bg-violet-900 bg-opacity-5 rounded-xl flex flex-col justify-start p-3 items-center h-[90%] gap-4">
+                
             <HiMiniUserCircle className="w-32 h-[30%] text-violet-800 " />
             {/* <MemoizedToastContainer/> */}
+
+
             <div className="gap-4 bg-violet-200 shadow-xl  w-full justify-center items-center h-[50%] rounded-xl flex flex-col ">
                 <div className=" lg:h-10 h-20 flex w-[80%] justify-start items-center text-violet-300 bg-violet-800 ">
-                    <FaUserEdit className="w-[20%] h-[40%]  "  />
-                    <input onChange={(e)=>handleChange(e)} value={user.firstname} type="text" name="firstname" placeholder="firstname" className=" focus:outline-none  p-1 text-sm border-s h-full w-[80%] bg-transparent" id="" />
+                    <FaUserEdit className="w-[20%] h-[40%]  " />
+                    <input onChange={(e) => handleChange(e)} value={user.firstname} type="text" name="firstname" placeholder="firstname" className=" focus:outline-none  p-1 text-sm border-s h-full w-[80%] bg-transparent" id="" />
                 </div>
                 <div className=" lg:h-10 h-20 flex w-[80%] text-violet-300 bg-violet-800 justify-start bg-transparent items-center  ">
-                    <MdAlternateEmail  className="w-[20%] h-[40%]  "  />
-                    <input onChange={(e)=>handleChange(e)}  type="email" name="email" placeholder="email" value={user.email} className=" focus:outline-none  p-1 text-sm border-s h-full w-[80%] bg-transparent" id="" />
+                    <MdAlternateEmail className="w-[20%] h-[40%]  " />
+                    <input onChange={(e) => handleChange(e)} type="email" name="email" placeholder="email" value={user.email} className=" focus:outline-none  p-1 text-sm border-s h-full w-[80%] bg-transparent" id="" />
                 </div>
                 <div className=" lg:h-10 h-20 flex w-[80%] justify-start text-violet-300 bg-violet-800 items-center ">
-                    <RiLockPasswordFill className="w-[20%] h-[40%]  "  />
-                    <input onChange={(e)=>handleChange(e)}  type="password" name="password" placeholder="password" value={user.password} className=" focus:outline-none  p-1 text-sm border-s h-full w-[80%] bg-transparent" id="" />
+                    <RiLockPasswordFill className="w-[20%] h-[40%]  " />
+                    <input onChange={(e) => handleChange(e)} type="password" name="password" placeholder="password" value={user.password} className=" focus:outline-none  p-1 text-sm border-s h-full w-[80%] bg-transparent" id="" />
                 </div>
             </div>
             <div className="gap-4 w-full lg:h-14 justify-center items-center   rounded-b-xl flex flex-col ">
-                    <button onClick={saveData} className=" bg-violet-200  rounded-lg w-24 justify-center items-center flex  border  h-10 -z-  ">
-                        login
-                    </button>
+                <button onClick={saveData} className=" bg-violet-200  rounded-lg w-24 justify-center items-center flex  border  h-10 -z-  ">
+                    login
+                </button>
             </div>
         </div>
     )
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,8 +3,30 @@ import LoginBox from "../components/login/loginBox"
 const Login = () => {
     return (
         <div className="w-full   h-[100%] justify-center relative flex items-center">
-            <div className="  w-[70%] h-[70%] flex justify-center items-center   xl:w-3/12 lg:end-12   rounded-md  ">
+            <div className="  w-[70%] h-[70%] flex justify-center items-center flex-col    xl:w-3/12 lg:end-12   rounded-md  ">
                 <LoginBox />
+                <div className="w-full max-w-md p-4 mx-auto bg-white rounded-lg shadow-md text-gray-800">
+                    <div className="mb-4">
+                        <h1 className="text-lg font-semibold border-b pb-2 mb-2">Admin</h1>
+                        <p className="text-sm">
+                            <span className="font-medium">Username:</span> sandeeppachat@gmail.com
+                        </p>
+                        <p className="text-sm">
+                            <span className="font-medium">Password:</span> Asd@123.com
+                        </p>
+                    </div>
+                    <div className="mb-4">
+                        <h1 className="text-lg font-semibold border-b pb-2 mb-2">Client</h1>
+                        <p className="text-sm">
+                            <span className="font-medium">Username:</span> sanooppachat@gmail.com
+                        </p>
+                        <p className="text-sm">
+                            <span className="font-medium">Password:</span> Asd@123.com
+                        </p>
+                    </div>
+                </div>
+
+
             </div>
         </div>
     )
