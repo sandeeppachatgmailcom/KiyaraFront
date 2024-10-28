a699e175dd72fbd0c0bd61c13601b5287c8c0cb0
sandeep <sandeepcheriyan@gmail.com>
2024-10-28 17:20:07 +0530

    graph interface modified

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,7 +9,7 @@ const Login = () => {
                     <div className="mb-4">
                         <h1 className="text-lg font-semibold border-b pb-2 mb-2">Admin</h1>
                         <p className="text-sm">
-                            <span className="font-medium">Username:</span> admin@gmail.com@gmail.com
+                            <span className="font-medium">Username:</span> admin@gmail.com
                         </p>
                         <p className="text-sm">
                             <span className="font-medium">Password:</span> Asd@123.com
